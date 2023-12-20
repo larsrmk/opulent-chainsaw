365ca21dcac1c8a7839cd17943caf6e36f5359fa
larsrmk <152874086+larsrmk@users.noreply.github.com>
2023-12-20 16:58:55 +0100

    Add files via upload
    update penrose triangle

--- penrose_triangle.py
+++ penrose_triangle.py
@@ -16,28 +16,68 @@ def penrose_triangle():
     for i in range(3):
         if i == 0:
             color("blue")
+            fillcolor("blue")
         if i == 1:
             color("pink")
+            fillcolor("pink")
         if i == 2:
             color("gold")
+            fillcolor("gold")
 
+        begin_fill()
         down()
         forward(120)
         left(60)
         forward(20)
         left(60)
-
         left(60)
         forward(100)
         right(120)
         forward(55)
-
+        left(60)
+        forward(20)
+        left(120)
+        forward(95)
+        end_fill()
+#Weg zurücklaufen
         up()
+        backward(95)
+        right(120)
+        back(20)
+        right(60)
         backward(55)
         left(120)
         backward(100)
         right(60)
 
+#schwarzer Rand
+    for i in range(3):
+        color("black")
+        pensize(8)
+
+        down()
+        forward(120)
+        left(60)
+        forward(20)
+        left(120)
+        forward(100)
+        right(120)
+        forward(60)
+        left(60)
+        forward(20)
+        left(120)
+        forward(100)
+#Weg zurücklaufen
+        backward(100)
+        right(120)
+        backward(20)
+        right(60)
+        backward(60)
+        left(120)
+        backward(100)
+        right(60)
+
+    up()
     forward(60)
     left(90)
     forward(50)
@@ -45,10 +85,3 @@ def penrose_triangle():
     turtle.exitonclick()
 
 penrose_triangle()
-
-
-
-
-
-
-
