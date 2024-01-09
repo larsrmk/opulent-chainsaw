3bfbe2ac800a17a22184cafde814608ac51fcca4
larsrmk <152874086+larsrmk@users.noreply.github.com>
2024-01-09 23:07:07 +0100

    Add files via upload

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -4,16 +4,12 @@ def player_choice():
     print("1. rock\n2. paper\n3. scissors")
     return input("Choose 1/2/3: ")
 
-    
-
 def computer_choice():
     return str(random.randint(1, 3))
 
-
-
-def result(player_choice, computer_choice):
-    print("You selected {player_choice}.")
-    print("The computer selected {computer_choice}.")
+def result(pc, cc):
+    print(f"You selected {pc}.")
+    print(f"The computer selected {cc}.")
 
     if player_choice == computer_choice:
         return "Draw!"
@@ -28,7 +24,10 @@ if __name__ == "__main__":
     print("Welcome to rock, paper, scissors.")
 
     while True:
-        game = result(player_choice(), computer_choice())
+        pc = player_choice() 
+        cc = computer_choice()
+
+        game = result(pc, cc)
         print(game)
     
         repeat=input("One more game (y/n)? ").lower()
